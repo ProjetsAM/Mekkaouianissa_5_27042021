d58088465d9abfacba412a91caf8302327f71661
AnissaMEKKAOUI <adraris@gmail.com>
2021-10-03 09:53:06 +0200

    modif url API

--- front/js/apiFetch.js
+++ front/js/apiFetch.js
@@ -1,9 +1,5 @@
 console.log("projet kanap");
-const API_URL = "http://localhost:3000/images/kanap04.jpeg",
-"http://localhost:3000/images/kanap05.jpeg",
-"http://localhost:3000/images/kanap06.jpeg",
-"http://localhost:3000/images/kanap07.jpeg";
-"http://localhost:3000/images/kanap08.jpeg";
+const API_URL =  "http://localhost:3000/api/products";
 
 console.log(typeof API_URL)
 
