3c663fd4d305bd24caa08b2c73b35a44b63bacc6
AnissaMEKKAOUI <adraris@gmail.com>
2021-10-05 10:39:11 +0200

    modifs carts js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,8 @@
 onload = function() {
     console.log("je suis sur la page cart");
+    let totalItems = document.getElementById('totalQuantity');
+    totalItems.innerHTML = "<span>5</span>";
+    let totalPrice = document.getElementById('totalPrice');
+    totalPrice.innerHTML = "<span>420</span>";
 
 };
--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,3 +1,5 @@
 onload = function() {
     console.log("je suis sur la page confirmation");
+    let clientId = document.getElementById('orderId');
+    clientId.innerHTML = "<strong>Merci pour votre achat</strong>";
 };
