491058580ac8bb99118d74cb9778f02e29908335
AnissaMEKKAOUI <adraris@gmail.com>
2021-10-13 17:34:37 +0200

    script

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,21 @@
 onload = function() {
     console.log("je suis sur la page index");
 };
+
+const img = document.getElementById('img');
+
+fetch('http://localhost:3000/api/products')
+.then(res => {
+  console.log(res);
+
+  if(res.ok){
+    res.json().then(data => 
+        console.log(data[0].name));
+    }
+  } else {
+      console.log("ERREUR"),
+      document.getElementById('items').innerHTML = "Une erreur s'est produite, veuillez nous excuser"
+      
+  }
+}) 
+      
