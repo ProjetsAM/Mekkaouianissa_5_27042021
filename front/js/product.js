e9c3890fff893b8386118221c291d12f1ee6d04e
AnissaMEKKAOUI <adraris@gmail.com>
2021-10-22 10:19:43 +0200

    modifs

--- front/js/product.js
+++ front/js/product.js
@@ -4,12 +4,15 @@ onload = function() {
 // Récupérer l'url de la page courante et l'afficher dans un console.log
 //Est-ce que cette url a un paramètre qui indique quel identifiant a le produit? si oui: quelle est la valeur de cet identifiant ?
 //Essayer de récupérer en JS cet identifiant en se documentant sur URLSearchParams
+function getIdFromUrl() {
 
- const str = document.location.href;
- const url = new URL (str);
- const id = url.searchParams.get("id");
- console.log(window.location.href);
- console.log(id);
+    const str = document.location.href;
+    const url = new URL(str);
+    const id = url.searchParams.get("id");
+    console.log(window.location.href);
+    console.log(id);
 
+};
+getIdFromUrl ();
 
-
+ 
