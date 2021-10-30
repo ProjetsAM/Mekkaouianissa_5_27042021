3ac1d4e3141781616e2cdc4518bfef6888a6751a
AnissaMEKKAOUI <adraris@gmail.com>
2021-10-30 20:59:48 +0200

    modifs

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,6 @@
-onload = function() {
+  onload = function() {
     console.log("je suis sur la page product");
-    //const id;
+    let id;
 
     function getIdFromUrl() {
         const str = document.location.href;
@@ -8,15 +8,15 @@ onload = function() {
         id = url.searchParams.get("id");
     };
     getIdFromUrl();
-};
-
-const urlProduct = 'http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926';
-
-fetch(urlProduct)
-  .then((res) => res.json()) 
-  .then((data) => {
-      console.log(data);
-  
+   
+    const urlProduct = 'http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926';
+   
+    fetch(urlProduct)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        console.log(price);
     }
-  )
+    )
+};
  
