12240ce2a171bf3391f476a6b7d1f6a3ffb42011
AnissaMEKKAOUI <adraris@gmail.com>
2021-11-06 08:55:39 +0100

    modifs

--- front/js/product.js
+++ front/js/product.js
@@ -10,6 +10,8 @@
     };
     getIdFromUrl();
    
+    // Affiche le produit sur la page
+
     const urlProduct = 'http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926';
    
     fetch(urlProduct)
@@ -17,7 +19,34 @@
     .then((data) => {
         console.log(data);
         console.log(data.price);
+        console.log(data.description);
+        console.log(data.name);
+
+        let name = data.name;
+        let description = data.description;
+        let imageUrl = data.imageUrl;
+        let altTxt = data.altTxt;
+        let colors = data.colors;
+        let price = data.price;
+        let id = data._id;
+
+       
+        let kanapName = document.getElementById("title");
+        let kanapDescription = document.getElementById("description");
+        let kanapPrice = document.getElementById("price");
+        let kanapImageUrl = document.querySelector(".item__img").innerHTML = `<img src=${imageUrl} alt="photo du canape">`;
+
+        kanapName.innerHTML = name;
+        kanapDescription.innerHTML = description;
+        kanapPrice.innerHTML = price;
+        
+        
+
+       
+        
+
+
     }
-    )
+    );
 };
  
